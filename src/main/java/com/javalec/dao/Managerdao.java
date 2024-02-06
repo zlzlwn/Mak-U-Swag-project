aa42442a265bde1c4af68654848949d0176abd70
tjdwlsaos22 <sjjin1999@naver.com>
2024-02-07 03:10:39 +0900

    24-01-07

--- src/main/java/com/javalec/dao/Managerdao.java
+++ src/main/java/com/javalec/dao/Managerdao.java
@@ -58,6 +58,47 @@ DataSource dataSource;
     }return new Managerdto(labels,data);
 }
 	
+	
+	public Managerdto view1() {
+		
+		ArrayList<String> labels1 = new ArrayList<>();
+	    ArrayList<Integer> data1 = new ArrayList<>();
+	    ArrayList<Integer> data2 = new ArrayList<>();
+	    Connection connection = null;
+	    
+	    String query="SELECT \r\n"
+	    		+ "    gender, \r\n"
+	    		+ "    COUNT(CASE WHEN gender = '남' THEN 1 END) AS male_count, \r\n"
+	    		+ "    COUNT(CASE WHEN gender = '여' THEN 1 END) AS female_count\r\n"
+	    		+ "FROM \r\n"
+	    		+ "    user \r\n"
+	    		+ "GROUP BY \r\n"
+	    		+ "    gender; ";
+	    
+	    try {
+	    	connection = dataSource.getConnection();
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        Statement stmt_mysql = connection.createStatement();
+	        ResultSet rs = stmt_mysql.executeQuery(query);
+
+	        // ResultSet에서 데이터 추출하여 ArrayList에 추가
+	        while (rs.next()) {
+	        	String gender = rs.getString("gender");
+	            int maleCount = rs.getInt("male_count");
+	            int femaleCount = rs.getInt("female_count");
+	            
+	            labels1.add(gender);
+	            data1.add(maleCount);
+	            data2.add(femaleCount);
+	            
+	            
+	      
+	        }
+	        connection.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }return new Managerdto(labels1,data1,data2);
+	}
 
 
 }
--- src/main/java/com/javalec/dto/Managerdto.java
+++ src/main/java/com/javalec/dto/Managerdto.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class Managerdto {
 	private ArrayList<String> labels;
+	private ArrayList<String> labels1;
     private ArrayList<Integer> data;
+    private ArrayList<Integer> data1;
+    private ArrayList<Integer> data2;
     
     
 	public Managerdto(ArrayList<String> labels, ArrayList<Integer> data) {
@@ -40,6 +43,44 @@ public class Managerdto {
     public Integer getData(int index) {
         return data.get(index);
     }
+
+
+	public Managerdto(ArrayList<String> labels1, ArrayList<Integer> data1, ArrayList<Integer> data2) {
+		super();
+		this.labels1 = labels1;
+		this.data1 = data1;
+		this.data2 = data2;
+	}
+
+
+	public ArrayList<String> getLabels1() {
+		return labels1;
+	}
+
+
+	public void setLabels1(ArrayList<String> labels1) {
+		this.labels1 = labels1;
+	}
+
+
+	public ArrayList<Integer> getData1() {
+		return data1;
+	}
+
+
+	public void setData1(ArrayList<Integer> data1) {
+		this.data1 = data1;
+	}
+
+
+	public ArrayList<Integer> getData2() {
+		return data2;
+	}
+
+
+	public void setData2(ArrayList<Integer> data2) {
+		this.data2 = data2;
+	}
 	
 	
     
