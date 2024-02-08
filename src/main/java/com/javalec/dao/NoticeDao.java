8fce25f349a55257c4869a939b603bb85f2a1509
ji-hwan park <ji-hwanpark@ji-hwanui-noteubug.local>
2024-02-09 00:37:04 +0900

    2024-02-09  notice paging 중간 마무리
    2024-02-09  notice paging 설정한 값만큼 리스트 만드는거는 완료 하지만 리스트에서 2 누르면 오류

--- src/main/java/com/javalec/dao/NoticeDao.java
+++ src/main/java/com/javalec/dao/NoticeDao.java
@@ -31,20 +31,28 @@ DataSource dataSource;
 	
 	
 	
-		public ArrayList<NoticeDtoPJH> list(){
+		public ArrayList<NoticeDtoPJH> list(int requestPage, int numOfTuplePerPage){
 			ArrayList<NoticeDtoPJH> dtos= new ArrayList<NoticeDtoPJH>();
 			Connection connection = null;
 			PreparedStatement preparedStatement = null;
 			ResultSet resultSet = null;
+			int offset = (requestPage - 1) * numOfTuplePerPage;
 			
 			try {
 				connection = dataSource.getConnection();
-				String query="select noCategory, noTitle, noDate, noContent, noImage from notice;";
+				String query="SELECT noCategory, noTitle, noDate, noContent, noImage FROM notice LIMIT ?,?";
 				
 				preparedStatement = connection.prepareStatement(query);
+				
+				 preparedStatement.setInt(1, offset);
+			        preparedStatement.setInt(2, numOfTuplePerPage);
+			        
 				resultSet = preparedStatement.executeQuery();
 				
+				
+				
 				while(resultSet.next()) {
+					
 					String noCategory=resultSet.getString("noCategory");
 					String noTitle=resultSet.getString("noTitle");
 					String noDate=resultSet.getString("noDate");
@@ -75,7 +83,79 @@ DataSource dataSource;
 			}
 			return dtos;
 		}
-	
+		// countTuple 메서드 수정
+		public int countTuple() {
+		    Connection conn = null;
+		    PreparedStatement psmt = null;
+		    ResultSet rs = null;
+		    int count = 0;
+
+		    try {
+		        conn = dataSource.getConnection();
+		        String query = "SELECT COUNT(*) FROM notice";
+		        psmt = conn.prepareStatement(query);
+		        rs = psmt.executeQuery();
+
+		        if (rs.next()) {
+		            count = rs.getInt(1);
+		            System.out.println("list-count success");
+		        }
+
+		    } catch (Exception e) {
+		        System.out.println("list-count fail");
+		        e.printStackTrace();
+		    } finally {
+		        try {
+		            if (rs != null) rs.close();
+		            if (psmt != null) psmt.close();
+		            if (conn != null) conn.close();
+		            System.out.println("< rs, psmt, conn close success>");
+		        } catch (Exception e) {
+		            System.out.println("< rs, psmt, conn close Fail>");
+		        }
+		    }
+
+		    return count;
+		}
+
+		// content 메서드 추가
+		public NoticeDtoPJH content(int no) {
+		    NoticeDtoPJH dto = null;
+		    Connection conn = null;
+		    PreparedStatement psmt = null;
+		    ResultSet rs = null;
+		    
+		    try {
+		        conn = dataSource.getConnection();
+		        String query = "SELECT noCategory, noTitle, noDate, noContent, noImage FROM notice WHERE no = ?";
+		        psmt = conn.prepareStatement(query);
+		        psmt.setInt(1, no);
+		        rs = psmt.executeQuery();
+
+		        if (rs.next()) {
+		            String noCategory = rs.getString("noCategory");
+		            String noTitle = rs.getString("noTitle");
+		            String noDate = rs.getString("noDate");
+		            String noContent = rs.getString("noContent");
+		            String noImage = rs.getString("noImage");
+
+		            dto = new NoticeDtoPJH(noTitle, noCategory, noDate, noContent, noImage);
+		        }
+
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    } finally {
+		        try {
+		            if (rs != null) rs.close();
+		            if (psmt != null) psmt.close();
+		            if (conn != null) conn.close();
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		    }
+
+		    return dto;
+		}
 	
 	
 }
